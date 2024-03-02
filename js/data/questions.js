cc955920ad259cc9433d3c686b75bb73a2f52f33
“Kateryna <k.skoryna@gmail.com>
2024-03-02 19:04:54 +0100

    Add new questions to quiz

--- js/data/questions.js
+++ js/data/questions.js
@@ -1,29 +1,92 @@
 const questions = [
     {
-        question: "What is 2 + 2?",
+        question: "Inside which HTML element do we put the JavaScript?",
         answers: [
-            { text: "4", status: true },
-            { text: "22", status: false },
-            { text: "3", status: false },
-            { text: "14", status: false }
+            { text: "<script>", status: true },
+            { text: "<scripting>", status: false },
+            { text: "<javascript>", status: false },
+            { text: "<js>", status: false }
         ]
     },
     {
-        question: "What is 4 * 3?",
+        question: "How do you write \"Hello\xa0World\" in an alert box?",
         answers: [
-            { text: "2", status: false },
-            { text: "15", status: false },
-            { text: "122", status: false },
-            { text: "12", status: true }
+            { text: "alertBox(\"Hello\xa0World\");", status: false },
+            { text: "msg(\"Hello\xa0World\");", status: false },
+            { text: "msgBox(\"Hello\xa0World\");", status: false },
+            { text: "alert(\"Hello\xa0World\");", status: true }
         ]
     },
     {
-        question: "What is 5 * 5?",
+        question: "How do you create a function in JavaScript?",
         answers: [
-            { text: "2", status: false },
-            { text: "25", status: true },
-            { text: "13", status: false },
-            { text: "44", status: false }
+            { text: "function = myFunction()", status: false },
+            { text: "function myFunction()", status: true },
+            { text: "function:myFunction()", status: false },
+            { text: "function()", status: false }
+        ]
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        answers: [
+            { text: "if i = 5 then", status: false },
+            { text: "if (i == 5)", status: true },
+            { text: "if i = 5", status: false },
+            { text: "if i == 5 then", status: false }
+        ]
+    },
+    {
+        question: "How to write an IF statement for executing some code if \"i\" is NOT equal to 5?",
+        answers: [
+            { text: "if i <> 5", status: false },
+            { text: "if i =! 5 then", status: false },
+            { text: "if (i <> 5)", status: false },
+            { text: "if (i != 5)", status: true }
+        ]
+    },
+    {
+        question: "How does a FOR loop start?",
+        answers: [
+            { text: "for (i = 0; i <= 5)", status: false },
+            { text: "for (i <= 5; i++)", status: false },
+            { text: "for (i = 0; i <= 5; i++)", status: true },
+            { text: "for i = 1 to 5", status: false }
+        ]
+    },
+    {
+        question: "What is the correct way to write a JavaScript array?",
+        answers: [
+            { text: "function = myFunction()", status: false },
+            { text: "function myFunction()", status: true },
+            { text: "function:myFunction()", status: false },
+            { text: "function()", status: false }
+        ]
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        answers: [
+            { text: "const colors = \"red\", \"green\", \"blue\"", status: false },
+            { text: "const colors = (1:\"red\", 2:\"green\", 3:\"blue\")", status: false },
+            { text: "const colors = 1 = (\"red\"), 2 = (\"green\"), 3 = (\"blue\")", status: false },
+            { text: "const colors = [\"red\", \"green\", \"blue\"]", status: true }
+        ]
+    },
+    {
+        question: "How do you round the number 28.7, to the nearest integer?",
+        answers: [
+            { text: "Math.rnd(28.7)", status: false },
+            { text: "Math.round(28.7)", status: true },
+            { text: "rnd(28.7)", status: false },
+            { text: "round(28.7)", status: false }
+        ]
+    },
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        answers: [
+            { text: "onClick", status: false },
+            { text: "click", status: false },
+            { text: "onclick", status: true },
+            { text: "onMouseClick", status: false }
         ]
     }
 ];
